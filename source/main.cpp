e94153c9d8b0f04179edf6a9e9ec95acb26c6766
Nightkingale <63483138+Nightkingale@users.noreply.github.com>
2023-07-17 17:32:12 -0600

    Make a region information struct

--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,7 @@
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <coreinit/dynload.h>
 #include <coreinit/filesystem_fsa.h>
@@ -15,23 +17,30 @@
 
 #define BUFFER_SIZE 0x80000
 
-const char* nandFile = NULL;
+typedef struct {
+    const char* regionCode;
+    const char* nandFile;
+} RegionInfo;
+
 const char* dumpFile = "/vol/external01/dump.app";
 
-static bool checkIfTiramisu()
-{
-    OSDynLoad_Module mod;
-    if (OSDynLoad_Acquire("homebrew_kernel", &mod) == OS_DYNLOAD_OK) {
-        OSDynLoad_Release(mod);
-        return false;
-    }
-    return true;
-}
+RegionInfo regions[] = {
+    { "JPN", "slccmpt01:/title/00000001/00000002/content/0000001c.app"},
+    { "USA", "slccmpt01:/title/00000001/00000002/content/0000001f.app"},
+    { "EUR", "slccmpt01:/title/00000001/00000002/content/00000022.app"}
+};
 
-void printOnScreen(int line, const char* text)
+void printOnScreen(int line, const char* format, ...)
 {
-    OSScreenPutFontEx(SCREEN_TV, 0, line, text);
-    OSScreenPutFontEx(SCREEN_DRC, 0, line, text);
+    char buffer[256];
+    va_list args;
+    // Format the string into a buffer.
+    va_start(args, format);
+    vsnprintf(buffer, sizeof(buffer), format, args);
+    va_end(args);
+    // Print the string to the screen.
+    OSScreenPutFontEx(SCREEN_TV, 0, line, buffer);
+    OSScreenPutFontEx(SCREEN_DRC, 0, line, buffer);
 }
 
 void printErrorMenu()
@@ -51,7 +60,7 @@ void printErrorMenu()
     OSScreenFlipBuffersEx(SCREEN_DRC);
 }
 
-void printMainMenu()
+void printMainMenu(const RegionInfo* region)
 {
     OSScreenClearBufferEx(SCREEN_TV, 0);
     OSScreenClearBufferEx(SCREEN_DRC, 0);
@@ -60,31 +69,10 @@ void printMainMenu()
     printOnScreen(1, "Created by Nightkingale, on behalf of the UWUVCI-PRIME team");
     printOnScreen(2, "-----------------------------------------------------------");
 
-    // Check to see if 0000001c.app, 0000001f.app, or 00000022.app exist on the NAND.
-    if (fopen("slccmpt01:/title/00000001/00000002/content/0000001c.app", "r"))
-    {
-        nandFile = "slccmpt01:/title/00000001/00000002/content/0000001c.app";
-        printOnScreen(3, "Region Detected by Wii System Menu: JPN");
-        printOnScreen(4, "Wii Menu Theme File: 0000001c.app");
-    }
-    else if (fopen("slccmpt01:/title/00000001/00000002/content/0000001f.app", "r"))
-    {
-        nandFile = "slccmpt01:/title/00000001/00000002/content/0000001f.app";
-        printOnScreen(3, "Region Detected by Wii System Menu: USA");
-        printOnScreen(4, "Wii Menu Theme File: 0000001f.app");
-    }
-    else if (fopen("slccmpt01:/title/00000001/00000002/content/00000022.app", "r"))
-    {
-        nandFile = "slccmpt01:/title/00000001/00000002/content/00000022.app";
-        printOnScreen(3, "Region Detected by Wii System Menu: EUR");
-        printOnScreen(4, "Wii Menu Theme File: 00000022.app");
-    }
-    else {
-        // Block the user from continuing if the Wii System Menu is not installed.
-        printErrorMenu();
-    }
+    if (region != NULL) {
+        printOnScreen(3, "Region Detected by Wii System Menu: %s", region->regionCode);
+        printOnScreen(4, "Wii Menu Theme File: %s", strrchr(region->nandFile, '/') + 1);
 
-    if (nandFile != NULL) {
         printOnScreen(5, "-----------------------------------------------------------");
         printOnScreen(6, "Press A to dump Wii System Menu assets.");
         printOnScreen(7, "Press B to restore Wii System Menu assets.");
@@ -93,6 +81,20 @@ void printMainMenu()
         OSScreenFlipBuffersEx(SCREEN_TV);
         OSScreenFlipBuffersEx(SCREEN_DRC);
     }
+    else {
+        // Block the user from continuing if the Wii System Menu is not installed.
+        printErrorMenu();
+    }
+}
+
+static bool checkIfTiramisu()
+{
+    OSDynLoad_Module mod;
+    if (OSDynLoad_Acquire("homebrew_kernel", &mod) == OS_DYNLOAD_OK) {
+        OSDynLoad_Release(mod);
+        return false;
+    }
+    return true;
 }
 
 int main()
@@ -120,20 +122,29 @@ int main()
     // Set some variables for the Wii U GamePad.
     VPADStatus input;
     VPADReadError error;
+    // Detect the Wii Menu's region to edit.
+    RegionInfo* region = NULL;
+    const int numRegions = sizeof(regions) / sizeof(regions[0]);
+    // Loop through the regions and find the one that exists.
+    for (int i = 0; i < numRegions; ++i) {
+        if (fopen(regions[i].nandFile, "r")) {
+            region = &regions[i];
+            break;
+        }
+    }
 
-    // Print the console header.
-    printMainMenu();
+    printMainMenu(region);
 
     while (WHBProcIsRunning())
     {
-        if (nandFile != NULL)
+        if (region != NULL)
             // Watch the Wii U GamePad for button presses.
             VPADRead(VPAD_CHAN_0, &input, 1, &error);
 
         if (input.trigger & VPAD_BUTTON_A)
         {
             WHBLogPrint("Beginning theme data dump...");
-            WHBLogPrintf("Source: %s", nandFile);
+            WHBLogPrintf("Source: %s", region->nandFile);
             WHBLogPrintf("Destination: %s", dumpFile);
             WHBLogPrint("----------------------------------------");
             WHBLogConsoleDraw();
@@ -141,7 +152,7 @@ int main()
             WHBLogPrint("Dumping theme file to SD card.");
             WHBLogConsoleDraw();
             // Open the theme file and dump it to the SD card.
-            FILE *theme = fopen(nandFile, "rb");
+            FILE *theme = fopen(region->nandFile, "rb");
             if (theme == NULL)
             {
                 WHBLogPrint("Error opening theme file!");
@@ -186,20 +197,20 @@ int main()
             }
             OSSleepTicks(OSMillisecondsToTicks(3000));
             WHBLogPrint("----------------------------------------");
-            printMainMenu();
+            printMainMenu(region);
         }
         else if (input.trigger & VPAD_BUTTON_B)
         {
             WHBLogPrint("Beginning theme data restore...");
             WHBLogPrintf("Source: %s", dumpFile);
-            WHBLogPrintf("Destination: %s", nandFile);
+            WHBLogPrintf("Destination: %s", region->nandFile);
             WHBLogPrint("----------------------------------------");
             WHBLogConsoleDraw();
 
             WHBLogPrint("Restoring theme file from SD card.");
             WHBLogConsoleDraw();
             // Open the theme file and restore it to the right place on SLCCMPT.
-            FILE *theme = fopen(nandFile, "wb");
+            FILE *theme = fopen(region->nandFile, "wb");
             if (theme == NULL)
             {
                 WHBLogPrint("Error opening theme file!");
@@ -244,7 +255,7 @@ int main()
             }
             OSSleepTicks(OSMillisecondsToTicks(3000));
             WHBLogPrint("----------------------------------------");
-            printMainMenu();
+            printMainMenu(region);
         }
     }
     Mocha_DeInitLibrary();
